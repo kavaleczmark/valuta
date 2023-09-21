b74933daece6fd94f811e9e1d65c81dfafc01020
Mark Kavalecz <kiskav12@gmail.com>
2023-09-21 19:38:26 +0200

    Added compareto to Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,10 @@ public class Main {
         Money money2=new Money(3.0, Currency.getInstance("USD"));
         System.out.println(money1.getValue() + " " +money1.getCurrency());
         System.out.println(money2.getValue() + " " +money2.getCurrency());
+        System.out.println(money1.compareTo(money2));
         money1.addMoney(money2);
         System.out.println(money1.getValue() + " " +money1.getCurrency());
         System.out.println(money2.getValue() + " " +money2.getCurrency());
+        System.out.println(money1.compareTo(money2));
     }
 }
