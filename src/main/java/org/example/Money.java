4bc1e3b0e7cc58233c31a63d52f9b64b259cab25
Mark Kavalecz <kiskav12@gmail.com>
2023-09-21 18:44:35 +0200

    Money class first clean up

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -3,9 +3,9 @@ import java.util.Currency;
 public class Money {
     public double value;
     public Currency currency;
-    public Money(double dValue, Currency c) {
-        this.value = dValue;
-        this.currency = c;
+    public Money(double value, Currency currency) {
+        this.value = value;
+        this.currency = currency;
     }
 
     public double getValue() {
@@ -16,29 +16,29 @@ public class Money {
         return currency;
     }
 
-    public Money add(Money moneyzToGiveMeh) {
+    public Money addMoney(Money moneyToBeAdded) {
         // Convert
-        if (!this.currency.equals(moneyzToGiveMeh.getCurrency())) { // If the two currency does not match
-            if (this.getCurrency().equals(Currency.getInstance("USD")) && moneyzToGiveMeh.getCurrency().equals(Currency.getInstance("HUF")))
-                moneyzToGiveMeh = new Money(moneyzToGiveMeh.value *0.0034, Currency.getInstance("USD"));
-            else if (this.getCurrency().equals(Currency.getInstance("HUF")) && moneyzToGiveMeh.getCurrency().equals(Currency.getInstance("USD")))
-                moneyzToGiveMeh = new Money(moneyzToGiveMeh.value *249.3, Currency.getInstance("HUF"));
-//            else if (this.what().equals(Currency.getInstance("ASD")) && moneyzToGiveMeh.what().equals(Currency.getInstance("USD")))
-//                moneyzToGiveMeh = new Money(moneyzToGiveMeh.val *249.3, Currency.getInstance("ASD"));
+        if (!this.currency.equals(moneyToBeAdded.getCurrency())) { // If the two currency does not match
+            if (this.getCurrency().equals(Currency.getInstance("USD")) && moneyToBeAdded.getCurrency().equals(Currency.getInstance("HUF")))
+                moneyToBeAdded = new Money(moneyToBeAdded.value *0.0034, Currency.getInstance("USD"));
+            else if (this.getCurrency().equals(Currency.getInstance("HUF")) && moneyToBeAdded.getCurrency().equals(Currency.getInstance("USD")))
+                moneyToBeAdded = new Money(moneyToBeAdded.value *249.3, Currency.getInstance("HUF"));
+//          else if (this.what().equals(Currency.getInstance("ASD")) && moneyToBeAdded.what().equals(Currency.getInstance("USD")))
+//                moneyToBeAdded = new Money(moneyToBeAdded.val *249.3, Currency.getInstance("ASD"));
             else return null;
         }
-        this.value += moneyzToGiveMeh.getValue(); // Add value of the parameter to this.val
+        this.value += moneyToBeAdded.getValue(); // Add value of the parameter to this.val
         return this;
     }
 
-    public Integer compareTo(Money moneyToCompare) {
-        if (!this.c.equals(moneyToCompare.what())) {
-            if (this.what().equals(Currency.getInstance("USD")) && moneyToCompare.what().equals(Currency.getInstance("HUF")))
-                moneyToCompare = new Money(moneyToCompare.val*0.0034, Currency.getInstance("USD"));
-            else if (this.what().equals(Currency.getInstance("HUF")) && moneyToCompare.what().equals(Currency.getInstance("USD")))
-                moneyToCompare = new Money(moneyToCompare.val*249.3, Currency.getInstance("HUF"));
+    public Integer compareTo(Money m) {
+        if (!this.currency.equals(m.getCurrency())) {
+            if (this.getCurrency().equals(Currency.getInstance("USD")) && m.getCurrency().equals(Currency.getInstance("HUF")))
+                m = new Money(m.value *0.0034, Currency.getInstance("USD"));
+            else if (this.getCurrency().equals(Currency.getInstance("HUF")) && m.getCurrency().equals(Currency.getInstance("USD")))
+                m = new Money(m.value *249.3, Currency.getInstance("HUF"));
             else return null;
         }
-        return Double.compare(this.how_much(), moneyToCompare.how_much());
+        return Double.compare(this.getValue(), m.getValue());
     }
 }
