84331c866ea56e175d6d068268895dc226e8ea72
Mark Kavalecz <kiskav12@gmail.com>
2023-09-21 18:58:29 +0200

    Updated currency rates

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -22,11 +22,9 @@ public class Money {
         // Convert
         if (!this.currency.equals(moneyToBeAdded.getCurrency())) { // If the two currency does not match
             if (this.getCurrency().equals(Currency.getInstance("USD")) && moneyToBeAdded.getCurrency().equals(Currency.getInstance("HUF")))
-                moneyToBeAdded = new Money(moneyToBeAdded.value *0.0034, Currency.getInstance("USD"));
+                moneyToBeAdded = new Money(moneyToBeAdded.value *0.0027, Currency.getInstance("USD"));
             else if (this.getCurrency().equals(Currency.getInstance("HUF")) && moneyToBeAdded.getCurrency().equals(Currency.getInstance("USD")))
-                moneyToBeAdded = new Money(moneyToBeAdded.value *249.3, Currency.getInstance("HUF"));
-//          else if (this.what().equals(Currency.getInstance("ASD")) && moneyToBeAdded.what().equals(Currency.getInstance("USD")))
-//                moneyToBeAdded = new Money(moneyToBeAdded.val *249.3, Currency.getInstance("ASD"));
+                moneyToBeAdded = new Money(moneyToBeAdded.value *363.76, Currency.getInstance("HUF"));
             else return null;
         }
         this.value += moneyToBeAdded.getValue(); // Add value of the parameter to this.val
@@ -36,9 +34,9 @@ public class Money {
     public Integer compareTo(Money m) {
         if (!this.currency.equals(m.getCurrency())) {
             if (this.getCurrency().equals(Currency.getInstance("USD")) && m.getCurrency().equals(Currency.getInstance("HUF")))
-                m = new Money(m.value *0.0034, Currency.getInstance("USD"));
+                m = new Money(m.value *0.0027, Currency.getInstance("USD"));
             else if (this.getCurrency().equals(Currency.getInstance("HUF")) && m.getCurrency().equals(Currency.getInstance("USD")))
-                m = new Money(m.value *249.3, Currency.getInstance("HUF"));
+                m = new Money(m.value *363.76, Currency.getInstance("HUF"));
             else return null;
         }
         return Double.compare(this.getValue(), m.getValue());
