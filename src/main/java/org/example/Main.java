6a1cfe413b4865dca3e72e0cb0d1d5cd748bcda2
Mark Kavalecz <kiskav12@gmail.com>
2023-09-21 18:59:13 +0200

    Added example code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.Currency;
+
 public class Main {
     public static void main(String[] args) {
-
+        Money money1=new Money(100.0, Currency.getInstance("HUF"));
+        Money money2=new Money(3.0, Currency.getInstance("USD"));
+        System.out.println(money1.getValue() + " " +money1.getCurrency());
+        System.out.println(money2.getValue() + " " +money2.getCurrency());
+        money1.addMoney(money2);
+        System.out.println(money1.getValue() + " " +money1.getCurrency());
+        System.out.println(money2.getValue() + " " +money2.getCurrency());
     }
 }
